41eb3315eab3b5848de5badcd61ca76d553e49af
tvanantwerp <tom@tomvanantwerp.com>
2017-07-31 13:06:33 -0400

    Basic 404 route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import USMap from './components/USMap';
 import State from './components/State';
 import Footer from './components/Footer';
+import FourOhFour from './components/FourOhFour'
 import SBTCIData from './data/SBTCI.json';
 import './App.css';
 
@@ -14,8 +15,11 @@ class App extends Component {
       <Router>
         <div>
           <Header />
-            <Route eaxct path="/" component={USMap} />
+          <Switch>
+            <Route exact path="/" component={USMap} />
             <Route path="/state/:stateId" component={State} />
+            <Route component={FourOhFour} />
+          </Switch>
           <Footer />
         </div>
       </Router>
