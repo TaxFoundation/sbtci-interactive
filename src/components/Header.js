91e9294d922819dad5297e1efae79fcab1c9fe8e
tvanantwerp <tom@tomvanantwerp.com>
2017-08-17 13:16:56 -0400

    Header and navbar styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../images/logo.svg';
 
 const Header = () => {
   return (
     <header className="sbtci-header">
-      <h1>SBTCI</h1>
-      <nav>
-        <Link to="/">Overview</Link>
-      </nav>
+      <div className="sbtci-header-container container">
+        <Link className="sbtci-header-logo" to="/">
+          <img src={ Logo } alt="State Business Tax Climate Index" />
+        </Link>
+        <div className="sbtci-header-social">
+          filler
+        </div>
+        <nav className="sbtci-header-nav">
+          <Link className="sbtci-header-nav-link" to="/">Rankings</Link>
+          <Link className="sbtci-header-nav-link" to="/">States</Link>
+          <Link className="sbtci-header-nav-link" to="/">Categories</Link>
+          <Link className="sbtci-header-nav-link" to="/">Methodology</Link>
+        </nav>
+      </div>
     </header>
   );
 }
