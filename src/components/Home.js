f9c1ddfc424356182595d82c6b8d52e0aa4568c2
tvanantwerp <tom@tomvanantwerp.com>
2017-08-18 13:53:52 -0400

    Home page social button styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,16 @@ class Home extends Component {
             <p>Filler</p>
           </div>
           <div className="sbtci-home-social sbtci-box">
-            <p>Filler</p>
+            <div className="sbtci-home-social-text">
+              <h2>Spread the Word!</h2>
+              <p>How does your state rank? Brag (or complain) to your friends!</p>
+            </div>
+            <div className="sbtci-home-social-buttons">
+              <a href="" className="sbtci-home-social-button sbtci-social--twitter">T</a>
+              <a href="" className="sbtci-home-social-button sbtci-social--facebook">F</a>
+              <a href="" className="sbtci-home-social-button sbtci-social--linkedin">L</a>
+              <a href="" className="sbtci-home-social-button sbtci-social--email">E</a>
+            </div>
           </div>
         </div>
         <hr />
