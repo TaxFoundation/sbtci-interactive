d288a2fd45fa9be43f8fecd9c99eec623b2bb64c
tvanantwerp <tom@tomvanantwerp.com>
2017-08-18 10:00:49 -0400

    Style state page social buttons

--- src/components/USState.js
+++ src/components/USState.js
@@ -14,15 +14,23 @@ class USState extends Component {
         <div
           className="sbtci-state-header"
         >
-          <h1>#{this.state.total.rank} {this.state.name}</h1>
+          <h1><span className="sbtci-state-header-rank">#{this.state.total.rank}</span> {this.state.name}</h1>
         </div>
         <div className="sbtci-state-content container">
           <div className="sbtci-box sbtci-state-table">
             <USStateTable stateData={this.state} />
           </div>
           <div className="sbtci-state-social sbtci-box">
-            <h2>Spread the Word!</h2>
-            <p>How does your state rank? Brag (or complain) to your friends!</p>
+            <div className="sbtci-state-social-text">
+              <h2>Spread the Word!</h2>
+              <p>How does your state rank? Brag (or complain) to your friends!</p>
+            </div>
+            <div className="sbtci-state-social-buttons">
+              <a href="" className="sbtci-state-social-button sbtci-social--twitter">Twitter</a>
+              <a href="" className="sbtci-state-social-button sbtci-social--facebook">Facebook</a>
+              <a href="" className="sbtci-state-social-button sbtci-social--linkedin">LinkedIn</a>
+              <a href="" className="sbtci-state-social-button sbtci-social--email">Email a Friend</a>
+            </div>
           </div>
           <div className="sbtci-state-neighbors sbtci-box">
             <h2 className="sbtci-box-heading">Neighboring States</h2>
