ad29f3738b795da39142be6e22eb6d47c9d42802
tvanantwerp <tom@tomvanantwerp.com>
2017-09-19 11:52:45 -0400

    State score formatting

--- src/components/USState.js
+++ src/components/USState.js
@@ -24,18 +24,19 @@ class USState extends Component {
         </div>
         <div className="sbtci-state-content container">
           <div className="sbtci-box sbtci-state-table">
-            <div className="sbtci-box-heading">{this.props.stateData.name}</div>
             <USStateTable taxTypes={this.props.taxTypes} stateData={this.props.stateData} />
           </div>
           <div className="sbtci-state-neighbors sbtci-box">
             <div className="sbtci-box-heading">Neighboring States</div>
             <div>Placeholder</div>
-            <Link to="/" className="sbtci-box-footer">See Full Map</Link>
           </div>
           <div className="sbtci-state-top sbtci-box">
             <div className="sbtci-box-heading">Top States</div>
             <div>Placeholder</div>
-            <Link to="/" className="sbtci-box-footer">See Rankings</Link>
+          </div>
+          <div className="sbtci-state-bottom sbtci-box">
+            <div className="sbtci-box-heading">Bottom States</div>
+            <div>Placeholder</div>
           </div>
           <div className="sbtci-state-social sbtci-box">
             <div className="sbtci-state-social-text">
@@ -63,11 +64,6 @@ class USState extends Component {
           <div className="sbtci-state-print sbtci-box">
             <p>Get a handy one-pager on your state!</p>
             <p>Print!</p>
-          </div>
-          <div className="sbtci-state-bottom sbtci-box">
-            <div className="sbtci-box-heading">Bottom States</div>
-            <div>Placeholder</div>
-            <Link to="/" className="sbtci-box-footer">See Rankings</Link>
           </div>
         </div>
       </div>
--- src/components/USStateTable.js
+++ src/components/USStateTable.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { format } from 'd3-format';
 
 class USStateTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = props.stateData;
-  }
-
   render() {
+    let rankChanges = this.props.taxTypes.map((t) => {
+      let delta = this.props.stateData[t.id].prevRank - this.props.stateData[t.id].rank;
+      let color = null;
+      if (delta < 0) { color = '#ff0022'; }
+      if (delta > 0) { color = '#22ee77'; }
+      return {
+        id: t.id,
+        delta: delta,
+        color
+      };
+    });
+
+    const scoreFormat = format('.3r')
+
     return (
       <table className="sbtci-table">
+        <caption className="sbtci-box-heading">{ this.props.stateData.name }</caption>
         <thead>
           <tr>
             <th>&nbsp;</th>
@@ -19,18 +30,36 @@ class USStateTable extends Component {
         </thead>
         <tbody>
           {this.props.taxTypes.map((t) => {
+            let rankChange = rankChanges.filter((c) => {
+              return c.id === t.id;
+            })[0];
+
             return (
               <tr>
                 <td style={{borderRight: `3px inset ${t.hex}`}}>
                   <Link
                     className="sbtci-table-link"
                     to={t.id === 'total' ? '/' : `/tax/${t.id}`}
-                    >
-                      {t.name}
-                    </Link>
-                  </td>
-                  <td>{ this.state[t.id].rank}</td>
-                  <td>{ this.state[t.id].value}</td>
+                  >
+                    {t.name}
+                  </Link>
+                </td>
+                <td style={{textAlign: 'right'}}>
+                  { `#${this.props.stateData[t.id].rank}` }
+                  <span
+                    className="sbtci-table-change"
+                    style={
+                      rankChange.delta !== 0
+                      ? {color: rankChange.color}
+                      : null
+                    }
+                  >
+                    {`(${rankChange.delta > 0 ? '+' : ''}${rankChange.delta})`}
+                  </span>
+                </td>
+                <td style={{textAlign: 'right'}}>
+                  { scoreFormat(this.props.stateData[t.id].value) }
+                </td>
               </tr>
             );
           })}
