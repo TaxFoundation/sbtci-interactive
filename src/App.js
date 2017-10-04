b84c52bd5d155b0b540c5fe5645354a616548833
tvanantwerp <tom@tomvanantwerp.com>
2017-10-04 15:57:47 -0400

    Add component descriptions

--- src/App.js
+++ src/App.js
@@ -9,12 +9,42 @@ import SBTCIData from './data/SBTCI.json';
 import USData from './data/us.json';
 
 const taxTypes = [
-  {name: 'Overall Rank', id: 'total', hex: '#007BC3'},
-  {name: 'Corporate Taxes', id: 'corporate', hex: '#009688'},
-  {name: 'Individual Taxes', id: 'individual', hex: '#4DAF4E'},
-  {name: 'Sales Taxes', id: 'sales', hex: '#FEC111'},
-  {name: 'Property Taxes', id: 'propertyTax', hex: '#EF4438'},
-  {name: 'Unemp. Insur. Taxes', id: 'unemployment', hex: '#903F98'},
+  {
+    name: 'Overall Rank',
+    id: 'total',
+    hex: '#007BC3',
+    description: ''
+  },
+  {
+    name: 'Corporate Taxes',
+    id: 'corporate',
+    hex: '#009688',
+    description: 'The corporate tax component measures impacts of states’ major taxes on business activities, both corporate income and gross receipts taxes.'
+  },
+  {
+    name: 'Individual Taxes',
+    id: 'individual',
+    hex: '#4DAF4E',
+    description: 'The individual income tax component of the Index measures the impact of state and local taxes that fall on pass-through businesses.'
+  },
+  {
+    name: 'Sales Taxes',
+    id: 'sales',
+    hex: '#FEC111',
+    description: 'The sales tax component measures the impact of both sales and excise taxes, particularly when they fall upon business inputs.'
+  },
+  {
+    name: 'Property Taxes',
+    id: 'propertyTax',
+    hex: '#EF4438',
+    description: 'The property tax component measures impacts of real and personal property, inventory, estate, inheritance, and other wealth taxes.'
+  },
+  {
+    name: 'Unemp. Insur. Taxes',
+    id: 'unemployment',
+    hex: '#903F98',
+    description: 'The unemployment insurance tax component measures the impact of state UI tax attributes, from schedules to charging methods, on businesses.'
+  },
 ];
 
 class App extends Component {
