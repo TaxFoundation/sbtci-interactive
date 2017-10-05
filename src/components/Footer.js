af36fdde7c0b4c25b375bd9912b1b2157b1e1682
tvanantwerp <tom@tomvanantwerp.com>
2017-10-05 11:25:37 -0400

    Begin work on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Footer = (props) => {
   return (
-    <footer>
-
+    <footer className="sbtci-footer">
+      <div className="container">
+        <div className="sbtci-footer-info">
+          <p>Tax Foundation</p>
+        </div>
+        <div className="sbtci-footer-links"></div>
+      </div>
     </footer>
   );
 };
