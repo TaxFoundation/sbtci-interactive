eaa35ee0014c81acec07054a0c88d20dd58b5dbe
Tom VanAntwerp <tom@tomvanantwerp.com>
2017-10-06 16:04:35 -0400

    Fixes #7

--- src/components/ShareBoxes.js
+++ src/components/ShareBoxes.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { fullName, ordinal } from './Helpers';
+import { IconFacebook, IconTwitter } from './SocialIcons';
+
+const Box = ({children}, context) => {
+  return (
+    <div className="sbtci-box sbtci-state-share-box">
+      <p className="sbtci-state-share-box-text">{children}</p>
+      <div className="sbtci-state-share-box-buttons">
+        <IconFacebook className="sbtci-social--facebook sbtci-state-share-box-button" fill="#ffffff" text="Share" />
+        <IconTwitter className="sbtci-social--twitter sbtci-state-share-box-button" fill="#ffffff" text="Tweet" />
+      </div>
+    </div>
+  );
+}
 
 const ShareBoxes = (props) => {
   const ranks = props.taxTypes.map((t) => {
@@ -18,8 +31,12 @@ const ShareBoxes = (props) => {
 
   return (
     <div>
-      <div>{props.stateData.name} ranks {ordinal(bestRank.rank)} for {fullName(bestRank.name).toLowerCase()}.</div>
-      <div>{props.stateData.name} ranks {ordinal(worstRank.rank)} for {fullName(worstRank.name).toLowerCase()}.</div>
+      <Box>
+        {props.stateData.name} ranks {ordinal(bestRank.rank)} for {fullName(bestRank.name).toLowerCase()}.
+      </Box>
+      <Box>
+        {props.stateData.name} ranks {ordinal(worstRank.rank)} for {fullName(worstRank.name).toLowerCase()}.
+      </Box>
     </div>
   );
 };
