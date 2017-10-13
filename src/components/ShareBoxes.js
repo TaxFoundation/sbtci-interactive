c56a2c96553cd4c31aa41511a21d0e64313c02d8
Tom VanAntwerp <tom@tomvanantwerp.com>
2017-10-13 09:11:40 -0400

    Social share updates

--- src/components/ShareBoxes.js
+++ src/components/ShareBoxes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fullName, ordinal } from './Helpers';
+import { ordinal } from './Helpers';
 import { IconFacebook, IconTwitter } from './SocialIcons';
 import blurbs from '../data/blurbs.json';
 
@@ -7,7 +7,7 @@ const Box = (props) => {
   return (
     <div className="sbtci-box sbtci-state-share-box">
       <p className="sbtci-state-share-box-text">
-        {props.social === 'twitter' ? props.children : 'Share this page on Facebook.'}
+        {props.social === 'twitter' ? props.children : 'Share this state\'s 2018 State Business Tax Climate rankings with your Facebook friends!'}
       </p>
       <div className="sbtci-state-share-box-buttons">
         { props.social === 'twitter'
@@ -32,23 +32,9 @@ const Box = (props) => {
 }
 
 const ShareBoxes = (props) => {
-  const ranks = props.taxTypes.map((t) => {
-    let rank = props.stateData[t.id];
-    rank['name'] = t.name;
-    return rank;
-  });
-
-  const bestRank = ranks.reduce((prev, cur) => {
-    return prev.rank < cur.rank ? prev : cur;
-  });
-
-  const worstRank = ranks.reduce((prev, cur) => {
-    return prev.rank > cur.rank ? prev : cur;
-  });
-
-  const standardText = (name, item) => {
+  const standardText = (stateData) => {
     return {
-      text: `${name} ranks ${ordinal(item.rank)} for ${fullName(item.name).toLowerCase()}.`,
+      text: `${stateData.name} ranks ${ordinal(stateData.total.rank)} on the @taxfoundation 2018 Index`,
       hashtags: 'StateTaxReform'
     };
   }
@@ -60,8 +46,11 @@ const ShareBoxes = (props) => {
   let shareTexts = [];
 
   if (stateBlurbs) { stateBlurbs['blurbs'].map(b => shareTexts.push(b)); }
-  shareTexts.push(standardText(props.stateData.name, bestRank));
-  shareTexts.push(standardText(props.stateData.name, worstRank));
+  shareTexts.push(standardText(props.stateData, 'total'));
+  shareTexts.push({
+    text: 'See how your state\'s tax code stacks up on @taxfoundation\'s 2018 State Business Tax Climate Index',
+    hashtags: 'StateTaxReform' 
+  });
   
   shareTexts = shareTexts.slice(0, 2);
 
