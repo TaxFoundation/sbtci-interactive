b9a47f2fd5ed69ce423dc7afa2759c3d7bcfa3f3
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-09-07 12:28:44 -0400

    Add more variables to theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,20 @@
 const theme = {
   tfBlue: '#0094ff',
+  tabletWidth: 912,
+  desktopWidth: 1024,
+  maxWidth: 1200,
+  sbtciBlue: '#5fc2ff',
+  darkGray: '#333333',
+  gray: '#999999',
+  lightGray: '#cccccc',
+  green: '#00aa22',
+  lightGreen: '#89c589',
+  twitter: '#00b6f1',
+  facebook: '#3b5998',
+  linkedin: '#007bb6',
+  email: '#df4a32',
+  fontSize: 16,
+  radius: 4,
 };
 
 export default theme;
