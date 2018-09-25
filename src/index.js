61ee0c0f0311287486baf99a51ec1db8c0996e10
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-09-25 17:44:47 -0400

    Revert "Remove unused service worker"
    This reverts commit 4e9d8c95c3094cad1201b36c20b7c9c46ecc8f77.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-snapshot';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css/normalize.css';
 import './style.css';
 
 render(<App />, document.getElementById('root'));
+registerServiceWorker();
