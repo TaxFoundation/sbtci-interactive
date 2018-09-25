3a1ae47a58421184d8279535765e991fbfc0180d
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-09-25 17:49:35 -0400

    And one more replacement

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,7 +66,7 @@ class Header extends Component {
             </div>
             <a
               className="sbtci-header-nav-category"
-              href="/Tax Foundation SBTCI 2019.pdf"
+              href="https://files.taxfoundation.org/20180925174436/2019-State-Business-Tax-Climate-Index.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
