cefc693cc619e743bd2068f39fa0ec80ef11f5a3
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-09-25 13:05:18 -0400

    Change social years to 2019

--- src/components/ShareBoxes.js
+++ src/components/ShareBoxes.js
@@ -7,7 +7,7 @@ const Box = (props) => {
   return (
     <div className="sbtci-box sbtci-state-share-box">
       <p className="sbtci-state-share-box-text">
-        {props.social === 'twitter' ? props.children : 'Share this state\'s 2018 State Business Tax Climate rankings with your Facebook friends!'}
+        {props.social === 'twitter' ? props.children : 'Share this state\'s 2019 State Business Tax Climate rankings with your Facebook friends!'}
       </p>
       <div className="sbtci-state-share-box-buttons">
         { props.social === 'twitter'
@@ -29,15 +29,15 @@ const Box = (props) => {
       </div>
     </div>
   );
-}
+};
 
 const ShareBoxes = (props) => {
   const standardText = (stateData) => {
     return {
-      text: `${stateData.name} ranks ${ordinal(stateData.total.rank)} on the @taxfoundation 2018 Index`,
+      text: `${stateData.name} ranks ${ordinal(stateData.total.rank)} on the @taxfoundation 2019 Index`,
       hashtags: 'StateTaxReform'
     };
-  }
+  };
 
   const stateBlurbs = blurbs.filter((b) => {
     return b.id === props.stateData.id;
@@ -48,7 +48,7 @@ const ShareBoxes = (props) => {
   if (stateBlurbs) { stateBlurbs['blurbs'].map(b => shareTexts.push(b)); }
   shareTexts.push(standardText(props.stateData, 'total'));
   shareTexts.push({
-    text: 'See how your state\'s tax code stacks up on @taxfoundation\'s 2018 State Business Tax Climate Index',
+    text: 'See how your state\'s tax code stacks up on @taxfoundation\'s 2019 State Business Tax Climate Index',
     hashtags: 'StateTaxReform' 
   });
   
