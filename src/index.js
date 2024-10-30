2325b0fd6bbe2725c30092c76d101a700716e1e4
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-10-30 18:44:01 -0400

    Delete everything and update readme

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-snapshot';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'normalize.css/normalize.css';
-import './style.css';
-
-render(<App />, document.getElementById('root'));
-registerServiceWorker();
